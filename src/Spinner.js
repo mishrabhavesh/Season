05629f0ff1df2004b7f3e4ed8b6bdb80961243a7
mishrabhavesh <mishra.bhavesh@outlook.com>
2020-06-30 10:06:50 +0530

    season app

---
+++ src/Spinner.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Spinner = (props) => {
+	return ( 
+	<div className="ui active dimmer">
+	    <div className="ui big text loader">{props.message}</div>
+	  <p>We are accessing your loctaion</p>
+	</div>
+	);
+};
+
+Spinner.defaultProps = {
+	message: 'Loading......'
+}
+export default Spinner
